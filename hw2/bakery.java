896fe3c2fecad962d841770bae5c0ea927259a7b
Jacob Grunwald <jacob.grunwald@colorado.edu>
2017-02-08 15:39:51 -0700

    Copied basic bakery structure from book

--- hw2/bakery.java
+++ hw2/bakery.java
@@ -1,1 +1,21 @@
-bakery
+class bakery implements Lock {
+  boolean[] flag;
+  Label[] label;
+  public bakery (int n) {
+    flag = new boolean[n];
+    label = new Label[n];
+    for(int i = 0; i < n; i++) {
+      flag[i] = false;
+      label[i] = 0;
+    }
+  }
+  public void lock() {
+    int i = ThreadID.get();
+    flag[i] = true;
+    label[i] = max(label[0], ...,label[n-1]) + 1;
+    while((k != i;)(flag(k] && (label[k],k) << (label[i],i))) {};
+  }
+  public void unlock() {
+    flag[ThreadID.get()] = false;
+  }
+}
